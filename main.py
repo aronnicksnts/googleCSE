a80690f22b7d8abd3fd2a69a5ac8674697a4c594
aronnicksnts <aronnicksnts@gmail.com>
2023-02-20 19:38:58 +0800

    images saved in different directory

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from p_tqdm import p_map
 from itertools import repeat
 import pandas as pd
 from datetime import datetime
+from os import mkdir
 
 dataJSON = json.load(open('data.json'))
 
@@ -18,9 +19,11 @@ imageQuery = dataJSON['imageQuery']
 # Rounds down to the nearest 10s
 numberOfImages = dataJSON['numberOfImages'] - dataJSON['numberOfImages']%10
 
+# Time for CSV and logs and folder that the images would be stored in
+currTime = datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
 
 # Initialize logger
-logging.basicConfig(filename=f'logs/{datetime.now().strftime("%Y-%m-%d-%H-%M-%S")}.log', encoding='utf-8', level=logging.INFO)
+logging.basicConfig(filename=f'logs/{currTime}.log', encoding='utf-8', level=logging.INFO)
 logging.getLogger('googleapiclient.discovery_cache').setLevel(logging.ERROR)
 
 # Saves images into a folder named images; if it fails, logs url
@@ -28,12 +31,12 @@ logging.getLogger('googleapiclient.discovery_cache').setLevel(logging.ERROR)
 def save_image(imageData: dict, imageNumber: str):
     try:
         #Need to change to work with proxies
-        urllib.request.urlretrieve(imageData['link'], f'images/{imageData["query"]}_{imageNumber}.jpg')
+        urllib.request.urlretrieve(imageData['link'], f'images/{currTime}/{imageData["query"]}_{imageNumber}.jpg')
         logging.info(f'Saved image_{imageNumber}: {imageData["link"]}')
     except HTTPError as e:
         logging.error(f'Failed to save image_{imageNumber}: {imageData["link"]}')
 
-        
+
 # Removes unnecessary data from the response and returns the cleaned response
 def cleanse_data(response):
     unneededData = ['kind', 'htmlTitle', 'displayLink', 'snippet',
@@ -86,22 +89,17 @@ if __name__ == "__main__":
     with Pool(8) as p:
         print("Scraping Image Links")
         imageData = p.starmap(get_images_data, zip(repeat(imageQuery), repeat(10), pageNumbers))
-
         allImageData = []
         # Changes dimension of imageData from [[imageDict, imageDict2]] to [imageDict, imageDict2]
         for images in imageData:
             allImageData.extend(images)
-        try:
-            currentCSV = pd.read_csv('data.csv')
-            imageMetaData = pd.concat([currentCSV, pd.DataFrame(allImageData)])
-            imageMetaData.reset_index(drop=True, inplace=True)
-        except:
-            imageMetaData = pd.DataFrame(allImageData)
-
+        
+        imageMetaData = pd.DataFrame(allImageData)
         print("Saving dataframe to CSV")
-        imageMetaData.to_csv('data.csv')
+        imageMetaData.to_csv(f'saved_metadata/{currTime}.csv')
         print("Finished Saving to CSV")
 
+        mkdir(f'images/{currTime}')
         print("Saving Images gathered")
         p.starmap(save_image, zip(allImageData, imageNumber))
         print("Finished Saving all Images")
